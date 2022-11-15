269174fe5f86b61611cf12066aa8a006a8e6d070
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-11-15 19:03:40 -0300

    feat: implemented dataValidation in User service

--- backend/src/Services/UserService.ts
+++ backend/src/Services/UserService.ts
@@ -7,7 +7,7 @@ export enum MessageErrors {
   INTERNAL = 'Internal Server Error',
   NOT_FOUND = 'Not Found',
   BAD_REQUEST = 'Bad Request',
-  INVALID_USERNAME = 'Username already registered!',
+  INVALID_USERNAME = 'Invalid Username',
   INVALID_TOKEN = 'Invalid Token',
   INVALID_PASSWORD = 'Invalid Password',
   CONFLICT = 'Conflict',
@@ -18,8 +18,19 @@ class UserService extends Service<User, UserPayload> {
   create = async (data: UserPayload): Promise<User | ResponseError> => {
     const user = await this.model.getOne(data);
     if (user) return { error: MessageErrors.INVALID_USERNAME };
-    
+    const validation = this.dataValidation(data);
+    if (validation) return validation;
     return this.model.create(data);
+  };
+
+  private dataValidation = (data: UserPayload): undefined | ResponseError => {
+    if (data.username.length < 3) {
+      return { error: MessageErrors.INVALID_USERNAME };
+    }
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    if (!regex.test(data.password)) {
+      return { error: MessageErrors.INVALID_PASSWORD };
+    }
   };
 }
 
