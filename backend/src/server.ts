a51f296f8b0f21f2889e9f79b76b9a56ce068822
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-11-15 16:41:18 -0300

    feat: implemented dependency injections ins server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+import { Router } from 'express';
 import App from './app';
+import UserController from './controllers/UserController';
+import UserModel from './Models/UserModel';
+import CustomRouter from './Routes/router';
+import UserService from './Services/UserService';
 
 const server = new App();
+const router = Router();
+const prisma = new PrismaClient();
+
+const userModel = new UserModel(prisma);
+const userService = new UserService(userModel);
+const userController = new UserController(userService);
+const userRouter = new CustomRouter(router);
+
+userRouter.addRoute(userController);
+server.addRouter(userRouter.router);
 
 export default server;
