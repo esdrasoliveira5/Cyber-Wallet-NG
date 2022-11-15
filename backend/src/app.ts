5f8718197f72621ae034801809b7ea2884975e74
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-11-15 17:16:36 -0300

    feat: implemented handleErrors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,18 @@
 import express, { Router, Request, Response } from 'express';
+import HandleError from './middlewares/HandleError';
 
 require('express-async-errors');
 
 class App {
   public app: express.Application;
 
+  public handleError = new HandleError();
+
   constructor() {
     this.app = express();
     this.app.use(express.json());
+    this.app.use(this.handleError.genericError);
+
     this.app.get('/', async (_req: Request, resp: Response) => {
       resp.status(200).json({
         message: 'API OLINE!!',
