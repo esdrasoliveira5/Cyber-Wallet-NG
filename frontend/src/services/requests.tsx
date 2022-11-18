06201eaffc16a4cf2e4c82e140540e94f2670cc0
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-11-18 19:15:54 -0300

    feat: implemented services request

---
+++ frontend/src/services/requests.tsx
@@ -0,0 +1,150 @@
+import {
+  ResponseError,
+  Transaction,
+  TransactionPayload,
+  User,
+  UserLogin,
+  UserPayload,
+} from '../types';
+
+const URL_FETCH = 'http://localhost:3000';
+const APLICATION = 'application/json';
+
+async function login(data: UserPayload): Promise<UserLogin | ResponseError> {
+  try {
+    const response = await fetch(`${URL_FETCH}/login`, {
+      method: 'POST',
+      headers: {
+        Accept: APLICATION,
+        'Content-Type': APLICATION,
+      },
+      body: JSON.stringify(data),
+    });
+    const results: UserLogin = await response.json();
+    return results;
+  } catch (error) {
+    return { error } as ResponseError;
+  }
+}
+
+async function createUser(data: UserPayload): Promise<User | ResponseError> {
+  try {
+    const response = await fetch(`${URL_FETCH}/user`, {
+      method: 'POST',
+      headers: {
+        Accept: APLICATION,
+        'Content-Type': APLICATION,
+      },
+      body: JSON.stringify(data),
+    });
+    const results: User = await response.json();
+    return results;
+  } catch (error) {
+    return { error } as ResponseError;
+  }
+}
+
+async function getUser(user: string, token: string): Promise<User | ResponseError> {
+  try {
+    const response: Response = await fetch(`${URL_FETCH}/user/${user}`, {
+      method: 'GET',
+      headers: {
+        Accept: APLICATION,
+        'Content-Type': APLICATION,
+        Authorization: token,
+      },
+    });
+    const results: User = await response.json();
+    return results;
+  } catch (error) {
+    return { error } as ResponseError;
+  }
+}
+
+async function getAllUsers(token: string): Promise<User[] | ResponseError> {
+  try {
+    const response: Response = await fetch(`${URL_FETCH}/user`, {
+      method: 'GET',
+      headers: {
+        Accept: APLICATION,
+        'Content-Type': APLICATION,
+        Authorization: token,
+      },
+    });
+    const results: User[] = await response.json();
+    return results;
+  } catch (error) {
+    return { error } as ResponseError;
+  }
+}
+
+async function createTransaction(
+  data: TransactionPayload,
+  token: string,
+): Promise<Transaction | ResponseError> {
+  try {
+    const response: Response = await fetch(`${URL_FETCH}/transaction`, {
+      method: 'GET',
+      headers: {
+        Accept: APLICATION,
+        'Content-Type': APLICATION,
+        Authorization: token,
+      },
+      body: JSON.stringify(data),
+    });
+    const results: Transaction = await response.json();
+    return results;
+  } catch (error) {
+    return { error } as ResponseError;
+  }
+}
+
+async function getTransaction(
+  transaction: string,
+  token: string,
+): Promise<Transaction | ResponseError> {
+  try {
+    const response: Response = await fetch(`${URL_FETCH}/transaction/${transaction}`, {
+      method: 'GET',
+      headers: {
+        Accept: APLICATION,
+        'Content-Type': APLICATION,
+        Authorization: token,
+      },
+    });
+    const results: Transaction = await response.json();
+    return results;
+  } catch (error) {
+    return { error } as ResponseError;
+  }
+}
+
+async function getAllTransactions(
+  transaction: string,
+  token: string,
+): Promise<Transaction[] | ResponseError> {
+  try {
+    const response: Response = await fetch(`${URL_FETCH}/transaction`, {
+      method: 'GET',
+      headers: {
+        Accept: APLICATION,
+        'Content-Type': APLICATION,
+        Authorization: token,
+      },
+    });
+    const results: Transaction[] = await response.json();
+    return results;
+  } catch (error) {
+    return { error } as ResponseError;
+  }
+}
+
+export default {
+  login,
+  createUser,
+  getUser,
+  getAllUsers,
+  createTransaction,
+  getTransaction,
+  getAllTransactions,
+};
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,7 +1,49 @@
-export interface AppContextInterface {
-  logged: {
+export type AppContextInterface = {
+  logged?: {
     logged: boolean;
     id: string;
     username: string;
   };
-}
+};
+
+export type UserPayload = {
+  username: string;
+  password: string;
+};
+
+export type User = {
+  id: number;
+  username: string;
+  accountId: number;
+};
+
+export type UserAccount = {
+  id: number;
+  username: string;
+  account: {
+    id: number;
+    balance: number;
+  };
+};
+
+export type UserLogin = {
+  user: UserAccount;
+  token: string;
+};
+
+export type ResponseError = {
+  error: string;
+};
+
+export type TransactionPayload = {
+  value: number;
+  creditedUsername: string;
+};
+
+export type Transaction = {
+  id: number;
+  value: number;
+  createdAt: string;
+  debitedAccountId: number;
+  creditedAccountId: number;
+};
