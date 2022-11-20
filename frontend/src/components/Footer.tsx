ceff5ee9075cfb791e1cf24ce0c3ba5d6c54f531
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-11-20 17:30:01 -0300

    feat: implemented footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,23 @@
+import github from '../../resources/gitHub.png';
 import { FooterS } from '../styles';
 
 function Footer() {
   return (
     <FooterS>
-      <p>Footer</p>
+      <p>© Cyber Wallet by Esdras Oliveira</p>
+      <div>
+        <a href="https://linkedin.com/in/esdrasmoliveira" target="blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
+            alt="esdrasmoliveira"
+            height="30"
+            width="30"
+          />
+        </a>
+        <a href="https://github.com/esdrasoliveira5" target="blank" rel="noreferrer">
+          <img src={github} alt="esdrasm.oliveira" height="30" width="30" />
+        </a>
+      </div>
     </FooterS>
   );
 }
